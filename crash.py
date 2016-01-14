3720f160320b613cb6b5eb18d3c71bea42e7d7aa
Eddie Antonio Santos <easantos@ualberta.ca>
2016-01-13 18:19:12 -0700

    Insult Josh's Python code.

--- crash.py
+++ crash.py
@@ -6,12 +6,12 @@
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 2
 #  of the License, or (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -56,6 +56,7 @@ class Crash(dict):
         'cpu_arch' : 'cpu', # Mozilla
         'frames' : 'stacktrace', # Mozilla
     }
+    # Code review: can use a set()/frozenset() for this.
     breakapart = {
         'crash_info' : 1, # Mozilla
         'system_info' : 1, # Mozilla
@@ -69,7 +70,7 @@ class Crash(dict):
     def load_from_file(cls, path):
         import launchpad_crash
         crash_classes = [ launchpad_crash.LaunchpadCrash ]
-        
+
         crash = None
         for crash_class in crash_classes:
             try:
@@ -83,14 +84,22 @@ class Crash(dict):
         return crash
 
     def __setitem__(self, key, val):
+        # Translates key synonyms to their "canonical" key.
         synonyms = self.synonyms
         if key in synonyms:
             return super(Crash, self).__setitem__(synonyms[key], val)
         elif key in self.breakapart:
+            # Inline all keys from the assigned value to THIS dict.
             if isinstance(val, dict):
+                # Code review: use self.update() instead;
+                # though... Not sure if dict.update() uses __setitem__
+                # internally.
                 for key2 in val:
+                    # It's okay to recurse on this function using indexing
+                    # syntax: self[key2] = val[key2]
                     self.__setitem__(key2, val[key2])
             else:
+                # Code review: Is TypeError more semantically related?
                 raise ValueError("Expected a dict!")
         elif key == 'crashing_thread': # Mozilla
             if (isinstance(val, dict)):
@@ -100,11 +109,20 @@ class Crash(dict):
                 raise ValueError("Expected a dict!")
         else:
             return super(Crash, self).__setitem__(key, val)
-        
+
+    @property
+    def stacktrace(self):
+        return self['stacktrace']
+
     def normalize(self):
+        """
+        wat.
+        """
+        # Use self.keys() so that we can remove items (it is impossible to
+        # modify the dictionary during iteration).
         for key in self.keys():
+            # wat.
+            # UnboundLocalError: local variable 'value' referenced before assignment
             value = self[value]
             del self[value]
             self.__setitem__(key, value)
-    
-
