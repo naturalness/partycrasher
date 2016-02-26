f66f30a7e5516ae66882f86145e363822e3f8cc8
Eddie Antonio Santos <easantos@ualberta.ca>
2016-02-26 14:50:09 -0700

    Add Flask CORS as requirments.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 def slurp(filename):
     with open(filename) as text_file:
@@ -6,13 +6,13 @@ def slurp(filename):
 
 setup(
     name='Partycrasher',
-    version='0.1dev',
+    version='0.1.0-dev0',
     packages=['partycrasher',],
     author='Joshua Charles Campbell, Eddie Antonio Santos',
     license='GPLv3+',
     long_description=slurp('README.md'),
+    install_requires=slurp('requirements.txt').splitlines(),
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     ],
-    install_requires=slurp('requirements.txt').splitlines()
 )
