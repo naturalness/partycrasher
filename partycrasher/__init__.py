5e262ca1f2afc732b2c9d898ae8ecc464aea1a4a
Eddie Antonio Santos <easantos@ualberta.ca>
2016-02-27 00:24:50 -0700

    Make ElasticSearch properties in PartyCrasher use lazy connection;
    also, add file I forgot about...

--- partycrasher/__init__.py
+++ partycrasher/__init__.py
@@ -18,9 +18,35 @@ class PartyCrasher(object):
         self.esServers = self.config.get('DEFAULT', 'elastic').split()
         if len(self.esServers) < 1:
             self.esServers = ['localhost']
-        self.es = Elasticsearch(self.esServers)
-        self.bucketer = MLTCamelCase(thresh=4.0, lowercase=False, only_stack=False, index='crashes', es=self.es, name="bucket")
-        self.bucketer.create_index()
+        # self.es and self.bucketer are lazy properties.
+        self._es = None
+        self._bucketer = None
+
+    @property
+    def es(self):
+        if not self._es:
+            self._connect_to_elasticsearch()
+        return self._es
+
+    @property
+    def bucketer(self):
+        if not self._bucketer:
+            self._connect_to_elasticsearch()
+        return self._bucketer
+
+    def _connect_to_elasticsearch(self):
+        """
+        Actually connects to ElasticSearch.
+        """
+        self._es = Elasticsearch(self.esServers)
+        self._bucketer = MLTCamelCase(thresh=4.0,
+                                      lowercase=False,
+                                      only_stack=False,
+                                      index='crashes',
+                                      es=self.es,
+                                      name="bucket")
+        self._bucketer.create_index()
+        return self._es
 
     # TODO catch duplicate and return 303
     # TODO multi-bucket multi-threshold mumbo-jumbo
@@ -36,13 +62,13 @@ class PartyCrasher(object):
             return self.bucketer.assign_bucket(Crash(crash))
         except NotFoundError as e:
             raise Exception(' '.join([e.error, str(e.status_code), repr(e.info)]))
-        
+
     def get_crash(self, database_id):
         try:
             return ESCrash(database_id, index='crashes')
         except NotFoundError as e:
             raise KeyError(database_id)
-        
+
     def delcrash(database_id):
         # TODO: we have to call ES directly here, theres nothing in Crash/ESCrash or Bucketer to handle this case
         # maybe ESCrash(database_id).delete()
