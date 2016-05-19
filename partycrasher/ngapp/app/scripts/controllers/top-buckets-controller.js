d62270f95f856a4e83e11050aaba846fc637553b
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-18 22:52:04 -0600

    Update view to consume real data.

--- partycrasher/ngapp/app/scripts/controllers/top-buckets-controller.js
+++ partycrasher/ngapp/app/scripts/controllers/top-buckets-controller.js
@@ -15,28 +15,72 @@ angular.module('PartyCrasherApp')
   $routeParams,
   $httpParamSerializer
 ) {
-  var url = $location.url();
-  $scope.value = 'Loading...';
-  restService.getRest('/').then(function (response) {
-    var value = response.plain();
-    $scope.availableThresholds = value.config.thresholds;
-  });
-  restService.getRest(url).then(function (response) {
-    $scope.value = response.plain();
-    $scope.currentThreshold = $scope.value.threshold;
-    delete $scope.value.threshold;
-  });
-  var urlThreshold = $routeParams.threshold;
-  var urlProject = $routeParams.project;
-  $scope.currentThreshold = urlThreshold;
-  $scope.$watch(
-    function (scope) { return scope.currentThreshold; },
-    function (newThreshold, oldThreshold) {
-      if (newThreshold != oldThreshold) {
-        var newUrl = '/' + urlProject + '/buckets/'
-        + newThreshold + '?'
-        + $httpParamSerializer($location.search());
-        $location.url(newUrl);
+
+
+  /* TODO: derive this from $routeParams and an AJAX call. */
+  Object.assign($scope, {
+    since: '2012-05-19T00:00:00Z',
+    threshold: '4.0',
+    top_buckets: [
+      {
+        href: 'http://partycrasher.dev/ubuntu/buckets/4.0/add215ee-1fbe-45e5-9ef3-334dc16fcc3f',
+        id: 'add215ee-1fbe-45e5-9ef3-334dc16fcc3f',
+        project: 'ubuntu',
+        threshold: '4.0',
+        /* TODO: requires LAST SEEN. */
+        last_seen: '2012-07-26T07:20:50Z',
+        total: 3
+      },
+      {
+        href: 'http://partycrasher.dev/alan_parsons/buckets/4.0/06080d73-1d33-4969-9d5e-69263cc01752',
+        id: '06080d73-1d33-4969-9d5e-69263cc01752',
+        project: 'alan_parsons',
+        threshold: '4.0',
+        total: 1
+      },
+      {
+        href: 'http://partycrasher.dev/alan_parsons/buckets/4.0/0d32cf8a-c1f4-409c-b177-00e693f3dc28',
+        id: '0d32cf8a-c1f4-409c-b177-00e693f3dc28',
+        project: 'alan_parsons',
+        threshold: '4.0',
+        total: 1
+      },
+      {
+        href: 'http://partycrasher.dev/alan_parsons/buckets/4.0/10eca7cc-b6d6-44bb-b760-fed7f69c9e8f',
+        id: '10eca7cc-b6d6-44bb-b760-fed7f69c9e8f',
+        project: 'alan_parsons',
+        threshold: '4.0',
+        total: 1
       }
+    ]
+  });
+
+  if (Math.random() < 0) idunnolol();
+
+  function idunnolol() {
+    var url = $location.url();
+    $scope.value = 'Loading...';
+    restService.getRest('/').then(function (response) {
+      var value = response.plain();
+      $scope.availableThresholds = value.config.thresholds;
     });
+    restService.getRest(url).then(function (response) {
+      $scope.value = response.plain();
+      $scope.currentThreshold = $scope.value.threshold;
+      delete $scope.value.threshold;
+    });
+    var urlThreshold = $routeParams.threshold;
+    var urlProject = $routeParams.project;
+    $scope.currentThreshold = urlThreshold;
+    $scope.$watch(
+      (scope) => scope.currentThreshold,
+      function (newThreshold, oldThreshold) {
+        if (newThreshold != oldThreshold) {
+          var query = $httpParamSerializer($location.search());
+          var newUrl = `/${urlProject}/buckets/${newThreshold}?${query}`;
+          /* Navigate to this URL. */
+          $location.url(newUrl);
+        }
+      });
+  }
 });
