920b3171e309046c268b205e47ca30fad106b577
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-25 14:14:52 -0600

    Minor changes.

--- partycrasher/ngapp/app/scripts/controllers/nav-controller.js
+++ partycrasher/ngapp/app/scripts/controllers/nav-controller.js
@@ -8,7 +8,11 @@
  * Controller of the ngappApp
  */
 angular.module('PartyCrasherApp')
-.controller('NavController', function ($scope, $route, namedRouteService) {
+.controller('NavController', function (
+  $scope, $route, namedRouteService, DEFAULT_THRESHOLD
+) {
+  $scope.threshold = DEFAULT_THRESHOLD;
+
   $scope.isActive = function (name) {
     if (!$route.current) {
       return false;
