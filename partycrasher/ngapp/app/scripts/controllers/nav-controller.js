ab719a419a0d16166513595ffb7950252979c150
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-25 10:11:21 -0600

    Do not use route directive -- it does not respect base.

--- partycrasher/ngapp/app/scripts/controllers/nav-controller.js
+++ partycrasher/ngapp/app/scripts/controllers/nav-controller.js
@@ -8,12 +8,17 @@
  * Controller of the ngappApp
  */
 angular.module('PartyCrasherApp')
-.controller('NavController', function ($scope, $route) {
+.controller('NavController', function ($scope, $route, namedRouteService) {
   $scope.isActive = function (name) {
     if (!$route.current) {
       return false;
     }
 
     return $route.current.name === name;
+  };
+
+  $scope.urlFor = function (name, args) {
+    /* Reverse the link, but remove the leading forward slash. */
+    return namedRouteService.reverse(name, args).substr(1);
   };
 });
