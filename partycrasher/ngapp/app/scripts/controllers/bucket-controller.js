b9f9a21096017b56c3e711b39544d6dce75ecbea
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-25 22:02:40 -0600

    Misc changes.

--- partycrasher/ngapp/app/scripts/controllers/bucket-controller.js
+++ partycrasher/ngapp/app/scripts/controllers/bucket-controller.js
@@ -8,21 +8,21 @@
  * Controller of the ngappApp
  */
 angular.module('PartyCrasherApp')
-.controller('BucketController', function ($scope, $http, $routeParams) {
+.controller('BucketController', function ($scope, SAMPLE_BUCKET, $http, $routeParams) {
   var project = $routeParams.project || '*',
     /* no default threshold, since this is a SPECIFIC bucket. */
     threshold = $routeParams.threshold,
     id = $routeParams.id;
 
   $scope.loading = true;
-  
+
+  Object.assign($scope, SAMPLE_BUCKET);
+
+  /* Fetch the bucket. */
   $http.get(bucketURL({ project, threshold, id }))
     .then(({data}) => {
       $scope.loading = false;
-      Object.assign($scope, data);
-    })
-    .catch((data) => {
-      console.error(data);
+      false && Object.assign($scope, data);
     });
 
   function bucketURL({ project, threshold, id }) {
