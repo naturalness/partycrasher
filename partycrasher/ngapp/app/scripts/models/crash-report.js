48af02df4d6272136bc4d4754a7f6419d3336a29
Eddie Antonio Santos <easantos@ualberta.ca>
2016-05-26 11:38:58 -0600

    Do work on rendering the stack trace.

--- partycrasher/ngapp/app/scripts/models/crash-report.js
+++ partycrasher/ngapp/app/scripts/models/crash-report.js
@@ -1,53 +1,61 @@
 /**
  * Instantiate by dependency injection.
  *
- * function (Crash, $scope) { ... }
+ * function (CrashReport, $scope) { ... }
  */
 angular.module('PartyCrasherApp')
-.constant('CrashReport', class Crash {
-  constructor(rawCrash) {
-    this._raw = rawCrash;
+.provider('CrashReport', function (StackFrame) {
+  class CrashReport {
+    constructor(rawCrash) {
+      this._raw = rawCrash;
+    }
+
+    get id() {
+      return this._raw['database_id'];
+    }
+
+    get project() {
+      return this._raw['project'];
+    }
+
+    get href() {
+      return this._raw['href'];
+    }
+
+    get date() {
+      var isoDate = this._raw['date'].replace(/z$/i, '') + 'Z';
+      return new Date(Date.parse(isoDate));
+    }
+
+    get stackTrace() {
+      return this._raw['stacktrace'].map(rawFrame => new StackFrame(rawFrame));
+    }
+
+    /**
+     * @return an object of { report_id, project, score, href }.
+     */
+    get topMatch() {
+      return this._raw['buckets']['top_match'];
+    }
+
+    /*== Some fields used in aggregations. ==*/
+
+    get os() {
+      return this._raw['os'];
+    }
+
+    get version() {
+      return undefined;
+    }
+
+    get build() {
+      return undefined;
+    }
   }
 
-  get id() {
-    return this._raw['database_id'];
-  }
-
-  get project() {
-    return this._raw['project'];
-  }
-
-  get href() {
-    return this._raw['href'];
-  }
-
-  get date() {
-    var isoDate = this._raw['date'].replace(/z$/i, '') + 'Z';
-    return new Date(Date.parse(isoDate));
-  }
-
-  get stacktrace() {
-    return this._raw['stacktrace'];
-  }
-
-  /**
-   * @return an object of { report_id, project, score, href }.
-   */
-  get topMatch() {
-    return this._raw['buckets']['top_match'];
-  }
-
-  /*== Some fields used in aggregations. ==*/
-
-  get os() {
-    return this._raw['os'];
-  }
-
-  get version() {
-    return undefined;
-  }
-
-  get build() {
-    return undefined;
-  }
+  return {
+    $get: function() {
+      return CrashReport;
+    }
+  };
 });
---
+++ partycrasher/ngapp/app/scripts/models/stack-frame.js
@@ -0,0 +1,43 @@
+/**
+ * Instantiate by dependency injection.
+ *
+ * function (StackFrame, $scope) { ... }
+ */
+angular.module('PartyCrasherApp')
+.constant('StackFrame', class StackFrame {
+  constructor(rawFrame) {
+    this._raw = rawFrame;
+  }
+
+  get address() {
+    return this._raw['address'];
+  }
+
+  get filename() {
+    /* Remove line information. */
+    var filename = this._raw['file'];
+    if (!filename) {
+      return undefined;
+    }
+    return filename.replace(/:\d+$/, '');
+  }
+
+  get line() {
+    var filename = this._raw['file'];
+    if (!filename) {
+      return undefined;
+    }
+
+    /* Return only line information. */
+    var matches = filename.match(/:(\d+)$/);
+    return matches[1];
+  }
+
+  get module() {
+    return undefined;
+  }
+
+  get func() {
+    return this._raw['function'];
+  }
+});
