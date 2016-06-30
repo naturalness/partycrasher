4c0bc09d25f779b4500dbbc16d11b26326b5971b
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-30 12:08:32 -0600

    Fix various small things with searching.

--- partycrasher/ngapp/app/scripts/controllers/search-controller.js
+++ partycrasher/ngapp/app/scripts/controllers/search-controller.js
@@ -1,45 +1,53 @@
+'use strict';
+
+/**
+ * Manages the search page and its search results.
+ */
 angular.module('PartyCrasherApp')
 .controller('SearchController', function (
   $scope,
   $location,
   $routeParams,
   PartyCrasher,
-  CrashReport
+  CrashReport,
+  PROJECT_NAMES
 ) {
   var q = $routeParams.q;
   var project = $routeParams.project || '*';
 
+  $scope.projects = PROJECT_NAMES;
+
   $scope.search = search;
   $scope.search.query = q;
-  
+  $scope.search.project = project;
+
+  /* Do initial search. */
   search();
-  
+
   function search() {
     $scope.loading = true;
     var query = $scope.search.query;
-    var from = $location.search.from;
-    var size = $location.search.size;
+    var project = $scope.search.project;
+    var from = $location.search.from || 0;
+    var size = $location.search.size || 10;
 
-    
-    PartyCrasher.searchQuery({ project, q, from, size })
+    PartyCrasher.searchQuery({ project, q: query, from, size })
       .then(results => {
-          var hits = results;
-          var reports = _.map(hits, (c) => {return new CrashReport(c);});
-          $scope.hasResults = hits.length > 0;
-          $scope.reports = reports;
-          $scope.errorMessage = null;
-          $scope.loading = false;
+        var hits = results;
+        var reports = _.map(hits, (c) => {return new CrashReport(c);});
+        $scope.hasResults = hits.length > 0;
+        $scope.reports = reports;
+        $scope.errorMessage = null;
+        $scope.loading = false;
       }).catch(error => {
-          $scope.hasResults = false;
-          $scope.reports = null;
-          $scope.errorMessage = error.data.message;
-          $scope.loading = false;
+        $scope.hasResults = false;
+        $scope.reports = null;
+        $scope.errorMessage = error.data.message;
+        $scope.loading = false;
       });
 
     $location
       .search('q', query)
-      .path('/*/search');
+      .path(`/${project}/search`);
   }
-  
-  
 });
