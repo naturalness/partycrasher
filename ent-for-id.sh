e7d4f9c09299c984084d6b7f020c9e58f349f3ae
Eddie Antonio Santos <easantos@ualberta.ca>
2016-08-26 15:54:58 -0600

    Figure out entropy of all the things.

--- ent-for-id.sh
+++ ent-for-id.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Prints the ID and the stack trace entropy on a single line.
+
 report_id="$1"
 
 # Extract the THIRD field from ent's terse output.
-./recursion_info.py "$report_id" | ent -t | awk -F, 'NR == 2 { print $3 }'
+./recursion_info.py "$report_id" \
+    | ent -t \
+    | awk -F, 'NR == 2 { print "'"$report_id"'," $3 }'
--- get-ent.sh
+++ get-ent.sh
@@ -2,9 +2,9 @@
 
 filename="$1"
 
-if [ ! -x "$filename" ]; then
+if [ ! -f "$filename" ]; then
     echo "could not read $filename..."
-    return 1
+    exit 1
 fi
 
 tail -n +2 "$filename" | xargs -n 1 ./ent-for-id.sh
