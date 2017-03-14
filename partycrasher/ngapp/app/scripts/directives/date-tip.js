d78800474dd6635670f7eeb7f7f1f4247ce92a49
Joshua Charles Campbell <joshua2@ualberta.ca>
2017-03-14 17:07:47 -0600

    add date tooltip

---
+++ partycrasher/ngapp/app/scripts/directives/date-tip.js
@@ -0,0 +1,22 @@
+angular.module('PartyCrasherApp')
+.directive('dateTip', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attributes) {
+      var container = angular.element("<span class='dateparent'></span>")
+      var old = element
+      element = element.clone()
+      container.append(element)
+      var tipdiv = angular.element("<div class='datetip'>Examples:<br/>MM/DD/YY<br/>2 hours ago<br/>Fri, 12 Dec 2014 10:55:50<br/>2016</div>");
+      container.append(tipdiv);
+      element.on("focus", function() {
+        tipdiv.css("display", "block");
+      });
+      element.on("blur", function() {
+        tipdiv.css("display", "none");
+      });
+      container = old.replaceWith(container)
+    }
+  };
+});
+
