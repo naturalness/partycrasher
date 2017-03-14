ba04851eb042e141873607b60078cb3e3a33389f
Joshua Charles Campbell <joshua2@ualberta.ca>
2017-03-14 17:45:08 -0600

    cleanup

--- partycrasher/ngapp/app/scripts/directives/date-tip.js
+++ partycrasher/ngapp/app/scripts/directives/date-tip.js
@@ -3,13 +3,10 @@ angular.module('PartyCrasherApp')
   return {
     restrict: 'A',
     link: function(scope, element, attributes) {
-      model = element.attr('ng-model');
       var container = angular.element("<span class='dateparent'></span>");
       var old = element;
       element = element.clone();
-      element.attr('ng-model', 'ngModel');
       container.append(element);
-//       container.attr('ng-model', old.attr('ng-model'))
       var tipdiv = angular.element("<div class='datetip'>Examples:<br/>MM/DD/YY<br/>2 hours ago<br/>Fri, 12 Dec 2014 10:55:50<br/>2016<br/>8 am</div>");
       container.append(tipdiv);
       element.on("focus", function() {
