cbd1f083e69cb2eafa443d9d8f63a0faa2ebdfa8
Joshua Charles Campbell <joshua2@ualberta.ca>
2017-03-14 18:15:31 -0600

    fix bug

--- partycrasher/ngapp/app/scripts/directives/date-tip.js
+++ partycrasher/ngapp/app/scripts/directives/date-tip.js
@@ -1,13 +1,14 @@
 angular.module('PartyCrasherApp')
-.directive('dateTip', function() {
+.directive('dateTip', function($compile) {
   return {
     restrict: 'A',
     link: function(scope, element, attributes) {
       var container = angular.element("<span class='dateparent'></span>");
       var old = element;
       element = element.clone();
+      element.removeAttr('date-tip');
       container.append(element);
-      var tipdiv = angular.element("<div class='datetip'>Examples:<br/>MM/DD/YY<br/>2 hours ago<br/>Fri, 12 Dec 2014 10:55:50<br/>2016<br/>8 am</div>");
+      var tipdiv = angular.element("<div class='datetip'>Examples:<br/>MM/DD/YY<br/>2 hours ago<br/>Fri, 12 Dec 2014 10:55:50<br/>2016</div>");
       container.append(tipdiv);
       element.on("focus", function() {
         tipdiv.css("display", "block");
@@ -15,11 +16,8 @@ angular.module('PartyCrasherApp')
       element.on("blur", function() {
         tipdiv.css("display", "none");
       });
-      container = old.replaceWith(container);
+      old.replaceWith($compile(container)(scope));
     },
-    scope: {
-      false
-    }
   };
 });
 
